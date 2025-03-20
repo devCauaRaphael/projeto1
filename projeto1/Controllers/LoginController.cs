85571956bea00c417b0ec4fd773e443d299bd387
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-03-20 14:51:04 -0300

    criando o método post da view login

--- projeto1/Controllers/LoginController.cs
+++ projeto1/Controllers/LoginController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using projeto1.Repositorio;
 
 namespace projeto1.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Login(string senha, string email)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+            if (usuario != null && usuario.senha == senha) 
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Email ou senha inválidos");
+
             return View();
         }
     }
+
 }
